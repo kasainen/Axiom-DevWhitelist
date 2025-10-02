0f591957bb39600bb5032d22fefe31e988bd7a6d
kasainen <8lxd@proton.me>
2025-10-02 15:04:17 -0400

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,12 +1,7 @@
 Config = {}
 
--- Default table name; can be overridden via convar `devwl_table`
 Config.tableName = GetConvar('devwl_table', 'fivem_dev_whitelist')
-
--- TTL for cache entries in seconds
 Config.cacheTtlSeconds = 60
-
--- User-facing messages
 Config.messages = {
     missingDiscord = 'Discord not detected. Please make sure Discord is running and linked to FiveM.',
     notWhitelisted = 'You are not whitelisted for the Dev server. Please request access.',
